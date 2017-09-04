9652aa53e5e3b7246a832a3a008c896cc2d9ed10
川合 俊介 <Shunsuke_Kawai@jmas.co.jp>
2017-09-04 11:18:08 +0900

    プロジェクト ファイルを追加します。

---
+++ ColorNavigationBarIcon/ColorNavigationBarIcon.iOS/Renderers/ColorIconContentPage.cs
@@ -0,0 +1,27 @@
+﻿using ColorNavigationBarIcon.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(ColorIconContentPageRenderer))]
+namespace ColorNavigationBarIcon.iOS.Renderers
+{
+    public class ColorIconContentPageRenderer : PageRenderer
+    {
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            if (this.NavigationController == null) return;
+
+            var items = this.NavigationController.TopViewController.NavigationItem;
+
+            foreach (var item in items.RightBarButtonItems)
+            {
+                if (item.Image == null) continue;
+
+                item.Image = item.Image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
+            }
+        }
+    }
+}
